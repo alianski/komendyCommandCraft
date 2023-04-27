bef2ffc097aa44149f43a726c33ff02954597dd4
alianski <125598402+alianski@users.noreply.github.com>
2023-04-27 17:39:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function komenda(id){
         document.getElementById("line002").textContent = '2. [repeat] [unconditional] [needs redstone] /effect give @p minecraft:invisibility 1 1';
         document.getElementById("line003").textContent = '3. [chain] [unconditional] [always active] /tp @e[name=balto] @p';
         document.getElementById("line004").textContent = '4. [chain] [unconditional] [always active] /effect give @p minecraft:speed 1 1 true';
-        document.getElementById("line005").textContent = '5 (ten w tubie a tuby nie trzeba budować ale ja zbudowałem bo miałęm ochotę ;-)).';
+        document.getElementById("line005").textContent = '5 (ten w tubie a tuby nie trzeba budować ale ja zbudowałem bo miałem ochotę ;-)).';
         document.getElementById("line006").textContent = '[repeat] [unconditional] [needs redstone] /tp @e[name=balto] ~ ~1 ~ facing 66.73 73.00 -31.39';
         document.getElementById("line007").textContent = '6.[repeat] [unconditional] [always active] /effect give @e[name=balto] minecraft:fire_resistance 1 10 true';
         document.getElementById("line008").textContent = '7.[repeat] [unconditional] [always active] /effect give @e[name=balto] minecraft:absorption 1 10 true';
@@ -21,9 +21,9 @@ function komenda(id){
         document.getElementById("line010").textContent = '9.[repeat] [unconditional] [always active] /effect give @e[name=balto] minecraft:regeneration 1 10 true';
     }
     if (id == 3){
-        document.getElementById("line001").textContent = komenda1;
+        document.getElementById("line001").textContent = 'już wkrótce';
     }
     if (id == 4){
-        document.getElementById("line001").textContent = komenda1;
+        document.getElementById("line001").textContent = 'już wkrótce';
     }
 }
